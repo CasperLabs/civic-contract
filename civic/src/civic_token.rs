6b5ebd41cbe17fe06c7ee6244720c39fa4236fda
astro019 <dmitry.yakov7@gmail.com>
2022-01-15 01:21:18 +1000

    update contract package initialization

--- civic/src/civic_token.rs
+++ civic/src/civic_token.rs
@@ -19,8 +19,9 @@ use casper_contract::{
     unwrap_or_revert::UnwrapOrRevert,
 };
 use casper_types::{
-    contracts::NamedKeys, runtime_args, ApiError, CLType, CLTyped, CLValue, EntryPoint,
-    EntryPointAccess, EntryPointType, EntryPoints, Group, Key, Parameter, RuntimeArgs, URef, U256,
+    contracts::NamedKeys, runtime_args, ApiError, CLType, CLTyped, CLValue, ContractPackageHash,
+    EntryPoint, EntryPointAccess, EntryPointType, EntryPoints, Group, Key, Parameter, RuntimeArgs,
+    URef, U256,
 };
 
 mod gatekeeper_control;
@@ -230,21 +231,12 @@ fn revoke_admin() {
 
 #[no_mangle]
 pub extern "C" fn call() {
-    let (package_hash, access_token) = storage::create_contract_package_at_hash();
-    let mut named_keys = NamedKeys::new();
-    let contract_package_hash_wrapped = storage::new_uref(package_hash).into();
-    named_keys.insert(
-        "contract_package_hash".to_string(),
-        contract_package_hash_wrapped,
-    );
-    let (contract_hash, _) =
-        storage::add_contract_version(package_hash, get_entry_points(), named_keys);
-
     // Read arguments for the constructor call.
     let name: String = runtime::get_named_arg("name");
     let symbol: String = runtime::get_named_arg("symbol");
     let meta: Meta = runtime::get_named_arg("meta");
     let admin: Key = runtime::get_named_arg("admin");
+    let contract_name: String = runtime::get_named_arg("contract_name");
 
     // Prepare constructor args
     let constructor_args = runtime_args! {
@@ -254,33 +246,33 @@ pub extern "C" fn call() {
         "admin" => admin
     };
 
-    // Add the constructor group to the package hash with a single URef.
+    let (contract_hash, _) = storage::new_contract(
+        get_entry_points(),
+        None,
+        Some(String::from("contract_package_hash")),
+        None,
+    );
+
+    let package_hash = ContractPackageHash::new(
+        runtime::get_key("contract_package_hash")
+            .unwrap_or_revert()
+            .into_hash()
+            .unwrap_or_revert(),
+    );
+
     let constructor_access: URef =
         storage::create_contract_user_group(package_hash, "constructor", 1, Default::default())
             .unwrap_or_revert()
             .pop()
             .unwrap_or_revert();
 
-    // Call the constructor entry point
-    let _: () =
-        runtime::call_versioned_contract(package_hash, None, "constructor", constructor_args);
+    let _: () = runtime::call_contract(contract_hash, "constructor", constructor_args);
 
-    // Remove all URefs from the constructor group, so no one can call it for the second time.
     let mut urefs = BTreeSet::new();
     urefs.insert(constructor_access);
     storage::remove_contract_user_group_urefs(package_hash, "constructor", urefs)
         .unwrap_or_revert();
 
-    // Store contract in the account's named keys.
-    let contract_name: alloc::string::String = runtime::get_named_arg("contract_name");
-    runtime::put_key(
-        &format!("{}_package_hash", contract_name),
-        package_hash.into(),
-    );
-    runtime::put_key(
-        &format!("{}_package_hash_wrapped", contract_name),
-        contract_package_hash_wrapped,
-    );
     runtime::put_key(
         &format!("{}_contract_hash", contract_name),
         contract_hash.into(),
@@ -288,10 +280,6 @@ pub extern "C" fn call() {
     runtime::put_key(
         &format!("{}_contract_hash_wrapped", contract_name),
         storage::new_uref(contract_hash).into(),
-    );
-    runtime::put_key(
-        &format!("{}_package_access_token", contract_name),
-        access_token.into(),
     );
 }
 
@@ -303,6 +291,7 @@ fn get_entry_points() -> EntryPoints {
             Parameter::new("name", String::cl_type()),
             Parameter::new("symbol", String::cl_type()),
             Parameter::new("meta", Meta::cl_type()),
+            Parameter::new("admin", Key::cl_type()),
         ],
         <()>::cl_type(),
         EntryPointAccess::Groups(vec![Group::new("constructor")]),
